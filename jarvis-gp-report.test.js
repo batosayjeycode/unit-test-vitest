b8e017207d7b3f10d01d73f1d1a2332fe7105d97
Andy Juliadi <andy.juliadi@sociolla.com>
2024-09-24 11:50:12 +0700

    fix: simplify code

--- jarvis-gp-report.test.js
+++ jarvis-gp-report.test.js
@@ -1,6 +1,64 @@
 import { expect, describe, it } from "vitest";
 const Helpers = require("./helpers");
 
+const arrColumn = [
+  {
+    sortByColumn: "NMV BEFORE DISCOUNT AMOUNT",
+    url: "&order_by=nmv&order_by_type=DESC",
+    expected: { success: true },
+  },
+  {
+    sortByColumn: "DISC. BY SOCIOLLA AMOUNT",
+    url: "&order_by=total_discount_sociolla&order_by_type=DESC",
+    expected: { success: true },
+  },
+  {
+    sortByColumn: "DISC. BY SOCIOLLA BRAND",
+    url: "&order_by=total_discount_brand&order_by_type=DESC",
+    expected: { success: true },
+  },
+  {
+    sortByColumn: "VOUCHER AMOUNT",
+    url: "&order_by=total_voucher&order_by_type=DESC",
+    expected: { success: true },
+  },
+  {
+    sortByColumn: "NET REVENUE AMOUNT",
+    url: "&order_by=net_revenue&order_by_type=DESC",
+    expected: { success: true },
+  },
+  {
+    sortByColumn: "NET REVENUE CONT.",
+    url: "&order_by=net_revenue_cont&order_by_type=DESC",
+    expected: { success: true },
+  },
+  {
+    sortByColumn: "COGS AMOUNT",
+    url: "&order_by=cogs&order_by_type=DESC",
+    expected: { success: true },
+  },
+  {
+    sortByColumn: "SUPPORT PROMO AMOUNT",
+    url: "&order_by=total_support_promo&order_by_type=DESC",
+    expected: { success: true },
+  },
+  {
+    sortByColumn: "NET COGS AMOUNT",
+    url: "&order_by=net_cogs&order_by_type=DESC",
+    expected: { success: true },
+  },
+  {
+    sortByColumn: "GROSS PROFIT AMOUNT",
+    url: "&order_by=gross_margin&order_by_type=DESC",
+    expected: { success: true },
+  },
+  {
+    sortByColumn: "GROSS PROFIT CONT.",
+    url: "&order_by=gross_margin_cont&order_by_type=DESC",
+    expected: { success: true },
+  },
+];
+
 describe.only(`[${process.env.JARVIS_ENV}] Test Concurrent Jarvis GP Report`, () => {
   it.concurrent(
     "Fetch Data GP Report Summary, Filter: default",
@@ -36,63 +94,14 @@ describe.only(`[${process.env.JARVIS_ENV}] Test Concurrent Jarvis GP Report`, ()
 describe(`[${process.env.JARVIS_ENV}] Test Jarvis GP Report Sort By Column Feature, Filter: default`, () => {
   const url =
     "https://jarvis-api.sociolla.info/v2/gross-profit/summaries?start_date=2024-09-01&end_date=2024-09-09&t=1725875854913&skip=0&limit=10&page=1&length=10&groups[]=brand_name&export_label_group_by_option=Brand";
-  it.each([
-    {
-      sortByColumn: "NMV BEFORE DISCOUNT AMOUNT",
-      url: `${url}&order_by=nmv&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "DISC. BY SOCIOLLA AMOUNT",
-      url: `${url}&order_by=total_discount_sociolla&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "DISC. BY SOCIOLLA BRAND",
-      url: `${url}&order_by=total_discount_brand&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "VOUCHER AMOUNT",
-      url: `${url}&order_by=total_voucher&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "NET REVENUE AMOUNT",
-      url: `${url}&order_by=net_revenue&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "NET REVENUE CONT.",
-      url: `${url}&order_by=net_revenue_cont&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "COGS AMOUNT",
-      url: `${url}&order_by=cogs&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "SUPPORT PROMO AMOUNT",
-      url: `${url}&order_by=total_support_promo&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "NET COGS AMOUNT",
-      url: `${url}&order_by=net_cogs&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "GROSS PROFIT AMOUNT",
-      url: `${url}&order_by=gross_margin&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "GROSS PROFIT CONT.",
-      url: `${url}&order_by=gross_margin_cont&order_by_type=DESC`,
-      expected: { success: true },
-    },
-  ])(
+
+  const arrSortByColumn = arrColumn.map((el) => {
+    const newObj = { ...el };
+    newObj.url = `${url}${newObj.url}`;
+    return newObj;
+  });
+
+  it.each(arrSortByColumn)(
     "GP Report Sort By Column: $sortByColumn -> $expected",
     async ({ sortByColumn, url, expected }) => {
       try {
@@ -110,63 +119,14 @@ describe(`[${process.env.JARVIS_ENV}] Test Jarvis GP Report Sort By Column Featu
 describe(`[${process.env.JARVIS_ENV}] Test Jarvis GP Report Sort By Column Feature, Filter: some filter`, () => {
   const url =
     "https://jarvis-api.sociolla.info/v2/gross-profit/summaries?brands[]=alchemist+fragrance&brands[]=saff+%26+co&brands[]=glad2glow&start_date=2024-09-01&end_date=2024-09-04&t=1726124938357&skip=0&limit=10&page=1&length=10&groups[]=brand_type&groups[]=product_purchase_type&groups[]=brand_name&export_label_group_by_option=Brand+Type+%3E%3E+Product+Purchase+Type+%3E%3E+Brand&export_label_filter_option=Brand:+alchemist+fragrance+or+saff+%26+co+or+glad2glow";
-  it.each([
-    {
-      sortByColumn: "NMV BEFORE DISCOUNT AMOUNT",
-      url: `${url}&order_by=nmv&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "DISC. BY SOCIOLLA AMOUNT",
-      url: `${url}&order_by=total_discount_sociolla&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "DISC. BY SOCIOLLA BRAND",
-      url: `${url}&order_by=total_discount_brand&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "VOUCHER AMOUNT",
-      url: `${url}&order_by=total_voucher&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "NET REVENUE AMOUNT",
-      url: `${url}&order_by=net_revenue&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "NET REVENUE CONT.",
-      url: `${url}&order_by=net_revenue_cont&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "COGS AMOUNT",
-      url: `${url}&order_by=cogs&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "SUPPORT PROMO AMOUNT",
-      url: `${url}&order_by=total_support_promo&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "NET COGS AMOUNT",
-      url: `${url}&order_by=net_cogs&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "GROSS PROFIT AMOUNT",
-      url: `${url}&order_by=gross_margin&order_by_type=DESC`,
-      expected: { success: true },
-    },
-    {
-      sortByColumn: "GROSS PROFIT CONT.",
-      url: `${url}&order_by=gross_margin_cont&order_by_type=DESC`,
-      expected: { success: true },
-    },
-  ])(
+
+  const arrSortByColumn = arrColumn.map((el) => {
+    const newObj = { ...el };
+    newObj.url = `${url}${newObj.url}`;
+    return newObj;
+  });
+
+  it.each(arrSortByColumn)(
     "GP Report Sort By Column: $sortByColumn -> $expected",
     async ({ sortByColumn, url, expected }) => {
       try {
