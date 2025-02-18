b86c6f6eee748c3b8e7fafd97443e8d5d64a92af
Andy Juliadi <andy.juliadi@sociolla.com>
2025-02-18 13:24:52 +0700

    fix: update clear cache script

--- jarvis-clear-cache.test.js
+++ jarvis-clear-cache.test.js
@@ -1,122 +1,76 @@
-import { describe, it } from "vitest";
+import { expect, describe, it } from "vitest";
 const Helpers = require("./helpers");
 
-// clear cache all
-describe(`[${process.env.JARVIS_ENV}] Test Concurrent Jarvis Clear Cache List`, () => {
-  it.concurrent("Clear Cache List: Order State", async ({ expect }) => {
-    try {
-      const data = { success: true };
-      const result = await Helpers.getDataFromAxios(
-        "https://jarvis-api.sociolla.info/order-state/list?clear_cache=true"
-      );
-      expect(result).toMatchObject(data);
-      expect(result).toEqual(expect.not.objectContaining({ fromCache: true }));
-    } catch (e) {
-      expect(e.message).toBe("Unable to Clear Cache List: Order State");
-    }
+const urlClearRedisCache =
+  "https://jarvis-api.sociolla.info/user-roles/clear-cache-redis";
+// const urlClearRedisCache = "https://uat-jarvis-api.sociolabs.io/user-roles/clear-cache-redis";
+// const urlClearRedisCache = "http://localhost:8989/user-roles/clear-cache-redis";
+
+// const roles = [
+//   {
+//     _id: {
+//       $oid: "67b2e0f199e1a725bbf452f9",
+//     },
+//     user_applied: 1,
+//     role_name: "Store Manager SBP & PE3 (Offline Store - ID)",
+//     role_desc: "Store Manager SBP (Offline Store - ID)",
+//   },
+// ]; //coming form database
+// const arrParamNew = roles.map((el) => {
+//   const param = {
+//     country: "id", //id or vn
+//     originalUrl: "/v2/order/additional-fields",
+//     role: el.role_name,
+//   };
+//   const paramUrl = new URLSearchParams(param);
+//   return paramUrl.toString();
+// });
+
+const arrParam = [
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Brand+Innovation+Assistant+04+%28Brand+Innovation+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Brand+Innovation+Assistant+05+%28Brand+Innovation+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Account+Receivable+%28Finance+-+VN%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Store+Manager+SEP+%28Offline+Store+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Store+Manager+SMS+%28Offline+Store+-+ID%29%09",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Store+Manager+XPA+%28Offline+Store+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Store+Manager+SMU+%28Offline+Store+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Senior+Store+Manager+03+%28Offline+Retail+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Senior+Store+Manager+04+%28Offline+Retail+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Senior+Store+Manager+05+%28Offline+Retail+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Senior+Store+Manager+06+%28Offline+Retail+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Senior+Store+Manager+07+%28Offline+Retail+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Store+Manager+XSS+%28Offline+Store+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Training+Manager+%28Service+Excellence+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Tax+Manager+%28Tax+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Product+Manager+%28Finance+System+-+All%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Store+Manager+XPN+%28Offline+Store+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Brand+Innovation+Assistant+06+%28Brand+Innovation+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Retail+Expansion+Team+%28Offline+Retail+-+All%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Store+Manager+XPB+%28Offline+Store+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Store+Manager+SMM+%28Offline+Store+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Store+Manager+SGC+%26+PE2+%28Offline+Store+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Brand+Assistant+15+%28Commerce+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Store+Manager+SBE+%26+PE3+%28Offline+Store+-+ID%29",
+  "country=vn&originalUrl=%2Fv2%2Forder%2Fadditional-fields&role=Store+Manager+SBP+%26+PE3+%28Offline+Store+-+ID%29",
+];
+
+describe(`[${process.env.JARVIS_ENV}] Clear cache`, () => {
+  const arrUrl = arrParam.map((el) => {
+    const newObj = {};
+    newObj.url = `${urlClearRedisCache}?${el}`;
+    newObj.expected = { success: true };
+    return newObj;
   });
 
-  it.concurrent("Clear Cache List: Store", async ({ expect }) => {
-    try {
-      const data = { success: true };
-      const result = await Helpers.getDataFromAxios(
-        "https://jarvis-api.sociolla.info/store/list?clear_cache=true"
-      );
-      expect(result).toMatchObject(data);
-      expect(result).toEqual(expect.not.objectContaining({ fromCache: true }));
-    } catch (e) {
-      expect(e.message).toBe("Unable to Clear Cache List: Store");
+  it.each(arrUrl)(
+    "Clear cache URL: $url -> $expected",
+    async ({ url, expected }) => {
+      try {
+        const result = await Helpers.getDataFromAxios(url);
+        expect(result).toMatchObject(expected);
+      } catch (e) {
+        expect(e.message).toBe(`Unable clear cache URL: ${url}`);
+      }
     }
-  });
-
-  it.concurrent("Clear Cache List: Order Type", async ({ expect }) => {
-    try {
-      const data = { success: true };
-      const result = await Helpers.getDataFromAxios(
-        "https://jarvis-api.sociolla.info/order-type/list?clear_cache=true"
-      );
-      expect(result).toMatchObject(data);
-      expect(result).toEqual(expect.not.objectContaining({ fromCache: true }));
-    } catch (e) {
-      expect(e.message).toBe("Unable to Clear Cache List: Order Type");
-    }
-  });
-
-  it.concurrent("Clear Cache List: Platform", async ({ expect }) => {
-    try {
-      const data = { success: true };
-      const result = await Helpers.getDataFromAxios(
-        "https://jarvis-api.sociolla.info/platform/list?clear_cache=true"
-      );
-      expect(result).toMatchObject(data);
-      expect(result).toEqual(expect.not.objectContaining({ fromCache: true }));
-    } catch (e) {
-      expect(e.message).toBe("Unable to Clear Cache List: Platform");
-    }
-  });
-
-  it.concurrent("Clear Cache List: Order Payment", async ({ expect }) => {
-    try {
-      const data = { success: true };
-      const result = await Helpers.getDataFromAxios(
-        "https://jarvis-api.sociolla.info/order-payments?length=2000&clear_cache=true"
-      );
-      expect(result).toMatchObject(data);
-      expect(result).toEqual(expect.not.objectContaining({ fromCache: true }));
-    } catch (e) {
-      expect(e.message).toBe("Unable to Clear Cache List: Order Payment");
-    }
-  });
-
-  it.concurrent("Clear Cache List: Province", async ({ expect }) => {
-    try {
-      const data = { success: true };
-      const result = await Helpers.getDataFromAxios(
-        "https://jarvis-api.sociolla.info/provinces?length=2000&clear_cache=true"
-      );
-      expect(result).toMatchObject(data);
-      expect(result).toEqual(expect.not.objectContaining({ fromCache: true }));
-    } catch (e) {
-      expect(e.message).toBe("Unable to Clear Cache List: Province");
-    }
-  });
-
-  it.concurrent("Clear Cache List: City", async ({ expect }) => {
-    try {
-      const data = { success: true };
-      const result = await Helpers.getDataFromAxios(
-        "https://jarvis-api.sociolla.info/cities?length=2000&clear_cache=true"
-      );
-      expect(result).toMatchObject(data);
-      expect(result).toEqual(expect.not.objectContaining({ fromCache: true }));
-    } catch (e) {
-      expect(e.message).toBe("Unable to Clear Cache List: City");
-    }
-  });
-
-  it.concurrent("Clear Cache List: Fullfilment", async ({ expect }) => {
-    try {
-      const data = { success: true };
-      const result = await Helpers.getDataFromAxios(
-        "https://jarvis-api.sociolla.info/fullfilments?length=2000&clear_cache=true"
-      );
-      expect(result).toMatchObject(data);
-      expect(result).toEqual(expect.not.objectContaining({ fromCache: true }));
-    } catch (e) {
-      expect(e.message).toBe("Unable to Clear Cache List: Fullfilment");
-    }
-  });
-
-  it.concurrent("Clear Cache List: Additional Field", async ({ expect }) => {
-    try {
-      const data = { success: true };
-      const result = await Helpers.getDataFromAxios(
-        "https://jarvis-api.sociolla.info/b2c/order/additional-fields?&clear_cache=true"
-      );
-      expect(result).toMatchObject(data);
-      expect(result).toEqual(expect.not.objectContaining({ fromCache: true }));
-    } catch (e) {
-      expect(e.message).toBe("Unable to Clear Cache List: Additional Field");
-    }
-  });
+  );
 });
